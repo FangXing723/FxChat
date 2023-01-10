a8ab21a9851e54941ace133a2d49fc0fe5752233
洪鑫 顾 <370721413@qq.com>
2023-01-10 23:51:49 +0800

    11

--- FxChat.App.Server/App.xaml.cs
+++ FxChat.App.Server/App.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FxChat.App.Server.Views;
+using Prism.Ioc;
+using Prism.Unity;
 using System.Windows;
 
 namespace FxChat.App.Server
@@ -11,7 +8,16 @@ namespace FxChat.App.Server
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainView>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
     }
 }
---
+++ FxChat.Domain/SocketInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FxChat.Domain
+{
+    public class SocketInfo
+    {
+        public Guid Id;
+        public Socket Socket{ get; set; }
+    }
+}
